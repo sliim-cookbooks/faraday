b8e26d9bbfd2d6c81b92a4cc4d0f90c73545a41a
Sliim <sliim@mailoo.org>
2015-09-25 17:19:16 +0200

    Update metadata.rb and indent .kitchen.yml

--- metadata.rb
+++ metadata.rb
@@ -9,5 +9,12 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
 recipe 'faraday::default', 'Basic install of Faraday'
+recipe 'faraday::config', 'Configure Faraday for a specific user'
+recipe 'faraday::cscan', 'Install continuous scanning tool'
 
-supports 'debian'
+supports 'debian', '= 7.0'
+
+source_url 'https://github.com/sliim-cookbooks/faraday' if
+  respond_to?(:source_url)
+issues_url 'https://github.com/sliim-cookbooks/faraday/issues' if
+    respond_to?(:issues_url)
